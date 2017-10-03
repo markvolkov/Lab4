171430a6089db6e3202c3bfd6d4f0b9928efb9fb
Mark Volkov <markvolkov@Marks-MacBook-Pro.local>
2017-10-02 20:56:38 -0400

    initial commit

---
+++ cmake-build-debug/CMakeFiles/lab4AllFiles.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lab4AllFiles.dir/Lab4Tester.cpp.o"
+  "CMakeFiles/lab4AllFiles.dir/TestLib.cpp.o"
+  "CMakeFiles/lab4AllFiles.dir/ArrayLib.cpp.o"
+  "lab4AllFiles.pdb"
+  "lab4AllFiles"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lab4AllFiles.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
